bc1da14410d663f9d7182bd133a1eaedc4834378
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-09-09 22:20:24 +0100

    - Added capability to close when the user clicks away from the menu

--- src/js/drawer-menus.js
+++ src/js/drawer-menus.js
@@ -7,17 +7,33 @@ import { msQueryAll } from "making-stuffs-queries";
   const menus = msQueryAll(".stuffs-drawer-menu");
 
   function toggle() {
+    if (this === document) return;
+    
     const associatedMenu = Array.from(menus).find(
       (menu) => menu.id === this.getAttribute("aria-controls")
     );
+    
+    const outsideClickListener = (event) => {
+      if ( event.target === associatedMenu ) {
+        associatedMenu.onclick = null;
+        toggle.call(this);
+      };
+    };
+
     if (associatedMenu.getAttribute("aria-expanded") === "true") {
       associatedMenu.setAttribute("aria-expanded", "false");
       associatedMenu.blur();
+      associatedMenu.onclick = null;
+      document.removeEventListener('click', toggle);
       this.blur();
+      return;
     } else {
       associatedMenu.setAttribute("aria-expanded", "true");
       associatedMenu.focus();
+      associatedMenu.onclick = outsideClickListener;
+      document.addEventListener('click', toggle);
       this.blur();
+      return;
     }
   }
 
